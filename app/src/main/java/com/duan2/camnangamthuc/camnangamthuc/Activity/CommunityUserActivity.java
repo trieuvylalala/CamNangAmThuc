bff802d15cdcc78820af31c77f873e77112a30f0
buivietphii <buivietphii@gmail.com>
2018-11-17 17:03:41 +0700

    cập nhật NavigationView cho giao diện cộng đồng use

---
+++ app/src/main/java/com/duan2/camnangamthuc/camnangamthuc/Activity/CommunityUserActivity.java
@@ -0,0 +1,13 @@
+package com.duan2.camnangamthuc.camnangamthuc.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommunityUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_user);
+    }
+}
